7a0dc6bf9f2fd384dc42bd3c026526afe07dac11
bluesky <chenchongbiao@deepin.org>
2024-07-24 20:22:58 +0800

    feat: use mmdebstrap to build rootfs
    * 使用 mmdebstrap 制作根文件系统，定制化更小。
    * 添加多架构支持。
    * 修改为 nile 版本
    
    Log: new rootfs

--- build.sh
+++ build.sh
@@ -1,14 +1,47 @@
 #!/bin/bash
 
-set -e
+set -e -u
 
-apt update
+sudo apt update
 
+# 不进行交互安装
 export DEBIAN_FRONTEND=noninteractive
+ROOTFS=`mktemp -d`
+dist_version="nile"
+dist_name="openkylin"
+readarray -t REPOS < ./config/apt/sources.list
+PACKAGES=`cat ./config/packages.list/packages.list | grep -v "^-" | xargs | sed -e 's/ /,/g'`
+OUT_DIR=$(dirname "$(realpath "$0")")/rootfs
 
-apt install multistrap -y
+mkdir -p $OUT_DIR
+sudo apt install -y curl git mmdebstrap qemu-user-static usrmerge usr-is-merged binfmt-support systemd-container
+# 开启异架构支持
+sudo systemctl start systemd-binfmt
 
-multistrap -f yangtze.multistrap
+# 安装软件包签名公钥
+curl http://archive.build.openkylin.top/openkylin/pool/main/o/openkylin-keyring/openkylin-keyring_2022.05.12-ok1_all.deb --output openkylin-keyring.deb
+sudo apt install ./openkylin-keyring.deb && rm ./openkylin-keyring.deb
 
-cp sources.list /yangtze-rootfs/etc/apt/sources.list
-cp openkylin-anything.list /yangtze-rootfs/etc/apt/sources.list.d/openkylin-anything.list
+curl http://archive.build.openkylin.top/openkylin/pool/main/o/openkylin-archive-anything/openkylin-archive-anything_2023.02.06-ok4_all.deb --output openkylin-archive-anything.deb
+sudo apt install ./openkylin-archive-anything.deb && rm ./openkylin-archive-anything.deb
+
+for arch in amd64 arm64 riscv64; do
+    sudo mmdebstrap \
+        --hook-dir=/usr/share/mmdebstrap/hooks/merged-usr \
+        --include=$PACKAGES \
+        --components="main,cross,pty" \
+        --variant=minbase \
+        --architectures=${arch} \
+        --customize=./config/hooks.chroot/second-stage \
+        $dist_version \
+        $ROOTFS \
+        "${REPOS[@]}"
+
+    # 生成压缩包
+    pushd $OUT_DIR
+    rm -rf $dist_name-rootfs-$arch.tar.gz
+    sudo tar -zcf $dist_name-rootfs-$arch.tar.gz -C $ROOTFS .
+    # 删除临时文件夹
+    sudo rm -rf  $ROOTFS
+    popd
+done
